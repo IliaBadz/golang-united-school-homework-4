c8d40f424bc42a7bcb3acba4963c64d4c9f3cd84
IliaBadz <badzaghua.ilia@gmail.com>
2022-05-20 03:16:49 +0400

    add: homework-4

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,9 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -23,5 +26,79 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	var (
+		leftOperand  []rune
+		rightOperand []rune
+		leftNum      int
+		rightNum     int
+		pos          int
+	)
+
+	newStrSlice := strings.Fields(input)
+
+	if len(newStrSlice) == 0 {
+		err = fmt.Errorf("%w", errorEmptyInput)
+		return "", err
+	}
+
+	input = strings.Join(newStrSlice, "")
+
+	runes := []rune(input)
+
+	for i := 0; i < len(runes); i++ {
+		
+		// Firstly get left operand 
+		if len(leftOperand) == 0 {
+
+			// Add left operand's operator if such exists
+			if i == 0 && (runes[i] == '-' || runes[i] == '+') {
+				leftOperand = append(leftOperand, runes[i])
+				i += 1
+			}
+
+			// Iterate over runes appending every digit that belongs to left operand's integer until operator is met or string is finished
+			for pos = i; pos < len(runes) && ((runes[pos] >= '0' && runes[pos] <= '9') || (runes[pos] != '-' && runes[pos] != '+')); pos++ {
+				leftOperand = append(leftOperand, runes[pos])
+			}
+
+			// Convert string to integer or return an error
+			leftNum, err = strconv.Atoi(string(leftOperand))
+			if err != nil {
+				e := err.(*strconv.NumError)
+				err = fmt.Errorf("%w", e)
+				return "", err
+			}
+
+			// Update i variable, so that for the next iteration it starts with operator in the input string
+			i = pos - 1
+
+		} else if len(rightOperand) == 0 {
+			
+			// Append operator to the right operand
+			rightOperand = append(rightOperand, runes[i])
+
+			// Iterate over runes appending every digit that belongs to the right operand's integer until operator is met or string is finished
+			for j := i + 1; j < len(runes) && ((runes[j] >= '0' && runes[j] <= '9') || (runes[j] != '-' && runes[j] != '+')); j++ {
+				rightOperand = append(rightOperand, runes[j])
+			}
+
+			// Convert string to integer or return an error
+			rightNum, err = strconv.Atoi(string(rightOperand))
+			if err != nil {
+				e := err.(*strconv.NumError)
+				err = fmt.Errorf("%w", e)
+				return "", err
+			}
+		}
+	}
+
+	// Check if incorrect number of operands are provided
+	if len(rightOperand) == 0 || (len(leftOperand)+len(rightOperand)) < len(runes) {
+		err = fmt.Errorf("%w", errorNotTwoOperands)
+		return "", err
+	}
+
+	output = strconv.Itoa(leftNum + rightNum)
+
+	return output, nil
 }
